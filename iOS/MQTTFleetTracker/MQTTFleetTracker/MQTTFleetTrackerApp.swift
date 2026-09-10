74c01be20af85a261ad24fd83a0a55c5a1e4c1ce
kusalrajapaksha <kusalrajapaksha1997@gmail.com>
2026-09-10 18:01:35 +0200

    [FEATURE] --- Initial iOS project

---
+++ iOS/MQTTFleetTracker/MQTTFleetTracker/MQTTFleetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTTFleetTrackerApp.swift
+//  MQTTFleetTracker
+//
+//  Created by Kusal on 2026-09-10.
+//
+
+import SwiftUI
+
+@main
+struct MQTTFleetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
